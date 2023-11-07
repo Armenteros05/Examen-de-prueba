b63a551a65b9c8e7dc9cf6c976a278903c6e5d3d
Armenteros05 <sarmerod@myuax.com>
2023-11-07 11:45:30 +0100

    lio y caos absoluto :)

--- triplepitagorico.cpp
+++ triplepitagorico.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <cmath>
-//tras realizar el código en pruebas.cpp intento trasladarlo aquí y modificar la fórmula para que de el resultado esperado
+//tras realizar el código en pruebas.cpp intento trasladarlo aquí y modificar la fórmula para que de e
 int main() {
-    for (int a = -100; a <= 100; a++) {
-        for (int b = -100; b <= 100; b++) {
-            for (int c = -100; c <= 100; c++) {
-                if (std::sqrt(a^2 + b^2) == c) {
+    for (int a = 1; a <= 100; a++) {
+        for (int b = 1; b <= 100; b++) {
+            for (int c = 1; c <= 100; c++) {
+                if (std::sqrt(a * a + b * b) == c) {
                     std::cout << "Triple pitagorico encontrado: a = " << a << ", b = " << b << ", c = " << c << std::endl;
                 }
             }
@@ -14,7 +14,3 @@ int main() {
 
     return 0;
 }
-//El código cumple los requisitos para ser un triple pitagórico ya que cumple las siguientes características:
-//La formula empleada es la pedida: sqrt(a^2 + b^2) == c
-//Los valores de a, b y c son menores o iguales a 100 y se cumple que la suma de a y b siempre es mayor que c
-//Se utilizan los bucles esperados con el bucle for
