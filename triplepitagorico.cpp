0ce6c2a925ab52e34bde5850031de5b9b81f3652
Armenteros05 <sarmerod@myuax.com>
2023-11-07 11:10:50 +0100

    pruebas en otro .cpp

--- triplepitagorico.cpp
+++ triplepitagorico.cpp
@@ -5,7 +5,7 @@ int main() {
     for (int a = 1; a <= 100; a++) {
         for (int b = 1; b <= 100; b++) {
             for (int c = 1; c <= 100; c++) {
-                if (std::sqrt(a * a + b * b) == c) {
+                if (std::sqrt(a^2 + b^2) == c) {
                     std::cout << "Triple pitagorico encontrado: a = " << a << ", b = " << b << ", c = " << c << std::endl;
                 }
             }
