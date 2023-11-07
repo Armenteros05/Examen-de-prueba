9919418d0e9c589bf965e66151642584cb9ef0a2
Armenteros05 <sarmerod@myuax.com>
2023-11-07 12:02:20 +0100

    lio y caos absoluto :)

--- triplepitagorico.cpp
+++ triplepitagorico.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cmath>
-//tras realizar el código en pruebas.cpp intento trasladarlo aquí y modificar la fórmula para que de e
+//tras realizar el código en pruebas.cpp intento trasladarlo aquí
 int main() {
     for (int a = 1; a <= 100; a++) {
         for (int b = 1; b <= 100; b++) {
             for (int c = 1; c <= 100; c++) {
+                //no he conseguido cambiar la fórmula que pides y solo he conseguido trabajar con esta
                 if (std::sqrt(a * a + b * b) == c) {
                     std::cout << "Triple pitagorico encontrado: a = " << a << ", b = " << b << ", c = " << c << std::endl;
                 }
             }
         }
     }
-
+std::cout << "El programa aplica tambien para todos estos valores con signo negativo,el cuadrado de un numero negativo siempre es +" << std::endl;
     return 0;
 }
